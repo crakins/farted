b481f937f0e31b04c59a725fc3bf9d9573b92807
Christopher Akins <crakins@2012-MBP.tx.rr.com>
2020-10-12 22:13:32 -0500

    new icon and touchablehighlight buttons

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
-import { Audio } from 'expo-av';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  Alert,
+} from "react-native";
+import { Audio } from "expo-av";
 
 const fartSounds = {
-  one: require('./assets/sounds/f1.wav'),
-  two: require('./assets/sounds/f3.mp3')
+  one: require("./assets/sounds/f1.wav"),
+  two: require("./assets/sounds/f3.mp3"),
 };
 
 export default function App() {
-
-  const handlePlaySound = async fart => {
+  const handlePlaySound = async (fart) => {
     const soundObject = new Audio.Sound();
-  
+
     try {
       let source = fartSounds[fart];
       //let source = require('./assets/A.wav');
       await soundObject.loadAsync(source);
       await soundObject
         .playAsync()
-        .then(async playbackStatus => {
+        .then(async (playbackStatus) => {
           setTimeout(() => {
             soundObject.unloadAsync();
           }, playbackStatus.playableDurationMillis);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
     } catch (error) {
@@ -35,34 +41,34 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image 
-          source={require('./assets/images/fart.png')} 
+        <Image
+          source={require("./assets/images/fart.png")}
           style={styles.fartLogo}
         />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-          style={[styles.button, {backgroundColor: 'blue' }]}
-          onPress={() => handlePlaySound('one')}
+        <TouchableHighlight
+          style={[styles.button, { backgroundColor: "blue" }]}
+          onPress={() => handlePlaySound("one")}
         >
           <Text style={styles.buttonText}>Fart</Text>
-        </TouchableOpacity>
+        </TouchableHighlight>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-          style={[styles.button, {backgroundColor: 'green' }]}
-          onPress={() => handlePlaySound('two')}
+        <TouchableHighlight
+          style={[styles.button, { backgroundColor: "green" }]}
+          onPress={() => handlePlaySound("two")}
         >
           <Text style={styles.buttonText}>Baby Fart</Text>
-        </TouchableOpacity>
+        </TouchableHighlight>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity 
-          style={[styles.button, {backgroundColor: 'black' }]}
-          onPress={() => Alert.alert('Pfffffffffttttttt')}
+        <TouchableHighlight
+          style={[styles.button, { backgroundColor: "black" }]}
+          onPress={() => Alert.alert("Pfffffffffttttttt")}
         >
           <Text style={styles.buttonText}>Silent Fart</Text>
-        </TouchableOpacity>
+        </TouchableHighlight>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -72,29 +78,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 50
+    backgroundColor: "#fff",
+    marginTop: 50,
   },
   buttonContainer: {
     height: 40,
-    margin: 5
+    margin: 5,
   },
   button: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 18
+    color: "#fff",
+    fontSize: 18,
   },
   imageContainer: {
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   fartLogo: {
     marginVertical: 30,
     height: 234,
     width: 339,
-  }
+  },
 });
